f3c45d3badfb9858aaac6cfa0131950554dc6608
Manfred <manfred@arch-hp.localdomain>
2022-03-24 22:18:16 -0300

    styled components

---
+++ src/Components/Terminal/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+interface Props {}
+
+const Terminal = (props: Props) => {};
+
+const RootContainer = styled.div`
+  display: flex;
+  background-color: blue;
+`;
+
+export default Terminal;
