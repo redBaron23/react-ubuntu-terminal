f653126ae22cc9fe1610b6d4b560c929cbe9a919
Pato <patotomastoledo6@gmail.com>
2022-04-08 19:30:16 -0300

    add IP as user

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import UbuntuTerminal from './Components/UbuntuTerminal';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import UbuntuTerminal from "./Components/UbuntuTerminal";
 
 function App() {
+  const [ip, setIp] = useState<string>("");
+
+  useEffect(() => {
+    fetch("https://geolocation-db.com/json/")
+      .then((res) => res.json())
+      .then((data) => setIp(data.IPv4));
+  }, []);
+  
   return (
     <div className="App">
-      <UbuntuTerminal username="manfred"/>
+      <UbuntuTerminal username={ip} />
     </div>
   );
 }
